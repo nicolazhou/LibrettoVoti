0746a2b21550bd1040a122950b8c061f3cf04f42
nicolazhou <nicolazhou@localhost>
2023-03-06 12:41:31 +0100

    Aggiunto classe Voto

---
+++ src/main/java/it/polito/tdp/libretto/model/Voto.java
@@ -0,0 +1,71 @@
+package it.polito.tdp.libretto.model;
+
+import java.time.*;
+
+public class Voto {
+
+	private String nomeCorso;
+	private int punti; // da 18 a 31
+	private LocalDate dataEsame;
+	
+	
+	public Voto(String nomeCorso, int punti, LocalDate dataEsame) {
+		this.nomeCorso = nomeCorso;
+		this.punti = punti;
+		this.dataEsame = dataEsame;
+	}
+
+
+	public String getNomeCorso() {
+		return nomeCorso;
+	}
+
+
+	public void setNomeCorso(String nomeCorso) {
+		this.nomeCorso = nomeCorso;
+	}
+
+
+	public int getPunti() {
+		return punti;
+	}
+
+
+	public void setPunti(int punti) {
+		this.punti = punti;
+	}
+
+
+	public LocalDate getDataEsame() {
+		return dataEsame;
+	}
+
+
+	public void setDataEsame(LocalDate dataEsame) {
+		this.dataEsame = dataEsame;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Voto [nomeCorso=" + nomeCorso + ", punti=" + punti + ", dataEsame=" + dataEsame + "]";
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+
+	public boolean equalsCorsoPunti(Voto nuovo) {
+		
+		if(this.nomeCorso.compareTo(nuovo.getNomeCorso())==0 && this.punti == nuovo.getPunti())
+			return true;
+		return false;
+	}
+	
+	
+	
+}
